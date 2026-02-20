de13370ea89fdde1d3f8dba871fde60bbdef8813
Matin Fekri <fekridotwork@gmail.com>
2026-02-20 13:51:46 +0330

    feat:complete reservation_detail view

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Reservation
+from menu.models import FoodItem
+from reservations.models import ReservationFood  # your through model
 
-# Create your views here.
+def reservation_detail(request, pk):
+    reservation = get_object_or_404(Reservation, pk=pk)
+
+    # Food items related to this reservation
+    foods = reservation.reservation_foods.all()
+
+    # Price breakdown
+    table_price = reservation.time_slot.table.price_per_person * reservation.number_of_people
+    food_total = sum(item.final_price for item in foods)
+    total_price = table_price + food_total
+
+    context = {
+        "reservation": reservation,
+        "foods": foods,
+        "table_price": table_price,
+        "food_total": food_total,
+        "total_price": total_price,
+    }
+
+    return render(request, "reservations/reservation_detail.html", context)
+
+def add_food_to_reservation(request, reservation_id, food_id):
+    reservation = get_object_or_404(Reservation, pk=reservation_id)
+    food = get_object_or_404(FoodItem, pk=food_id)
+
+    ReservationFood.objects.create(
+        reservation=reservation,
+        food_item=food,
+        final_price=food.price
+    )
+
+    reservation.update_total_price()
+
+    return redirect("reservation_detail", pk=reservation_id)
